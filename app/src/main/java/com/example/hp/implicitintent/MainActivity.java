c4806fb787075527f33b917edaa3ff10a1f625e9
HP-HP\HP <akm.agkgmyat@gmail.com>
2017-11-03 17:40:09 +0630

    Implicit Intents completed!

--- app/src/main/java/com/example/hp/implicitintent/MainActivity.java
+++ app/src/main/java/com/example/hp/implicitintent/MainActivity.java
@@ -1,20 +1,45 @@
 package com.example.hp.implicitintent;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int MY_PERMISSIONS_REQUEST_CALL_PHONE = 100;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private int PICK_IMAGE_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this, this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -26,6 +51,100 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    @OnClick(R.id.btn_share_with_sharecompact)
+    public void onTapShareWithCompact() {
+        startActivity(Intent.createChooser(ShareCompat.IntentBuilder.from(this)
+                .setType("text/plain")
+                .setText("Hello Android")
+                .getIntent(), "Share"));
+    }
+
+    @OnClick(R.id.btn_navigate_on_map)
+    public void onTapNavigateOnMap() {
+        Uri locationToOpen = Uri.parse("geo:16.7745, 96.1588");
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(locationToOpen);
+        intent.setPackage("com.google.android.apps.maps");
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    @OnClick(R.id.btn_make_ph_call)
+    public void onTapMakePhCall() {
+        Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:09978071892"));
+        if (ActivityCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CALL_PHONE},
+                    MY_PERMISSIONS_REQUEST_CALL_PHONE);
+            return;
+        }
+        startActivity(intent);
+    }
+
+    @OnClick(R.id.btn_send_email)
+    public void onTapSendEmail() {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_EMAIL, "agkgmyat99@gmail.com");
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Introduction !");
+        intent.putExtra(Intent.EXTRA_TEXT, "Hi !");
+        startActivity(Intent.createChooser(intent, "Send Email using Intent"));
+    }
+
+    @OnClick(R.id.btn_take_pic_with_camera)
+    public void onTapTakePicWithCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @OnClick(R.id.btn_select_pic)
+    public void onTapSelectPicFromDev() {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.setType("image/*");
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+    @OnClick(R.id.btn_save_event)
+    public void onTapSaveEventOnCalendar() {
+        Calendar beginTime = Calendar.getInstance();
+        beginTime.set(2017, 11, 4, 11, 00);
+        Calendar endTime = Calendar.getInstance();
+        endTime.set(2017, 11, 4, 12, 30);
+
+        saveEventIntent(beginTime, endTime);
+    }
+
+    private void saveEventIntent(Calendar beginTime, Calendar endTime) {
+        Intent intent = new Intent(Intent.ACTION_INSERT)
+                .setData(CalendarContract.Events.CONTENT_URI)
+                .putExtra(CalendarContract.Events.TITLE, "PADC Graduation Day")
+                .putExtra(CalendarContract.Events.EVENT_LOCATION, "Phandeeyar")
+                .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, beginTime.getTimeInMillis())
+                .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, endTime.getTimeInMillis());
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+            //do action with the picture taken here...
+
+//            try {
+//                Bitmap photo = (Bitmap) data.getExtras().get("data");
+//                ivPics.setImageBitmap(photo);
+//            }catch (NullPointerException e){
+//                Toast.makeText(this,"NullPointerException",Toast.LENGTH_SHORT).show();
+//            }
+
+        }
     }
 
     @Override
